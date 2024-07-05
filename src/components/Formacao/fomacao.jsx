e310b8f7daeee250463fa62ee3ca91a59d4e190f
Math3uz7 <matheuspprudencio@gmail.com>
2024-07-05 14:17:08 -0300

    data

--- src/components/Formacao/fomacao.jsx
+++ src/components/Formacao/fomacao.jsx
@@ -7,8 +7,12 @@ function formacao() {
         <section className={Styles.formacao}>
             <h2 className={Styles.h2}>Formação</h2>
             <div className={Styles.mDiv}><img src={img} alt="" className={Styles.img}/></div>
+
+<section className={Styles.content}>
             <h3>Curso: Sitemas para internet</h3>
             <h3>Inicio: <span className={Styles.red}>2023</span> | Termino: <span className={Styles.red}>2025</span></h3>
+</section>
+
             <hr />
         </section>
     )
